a7bde5b7ee6d75a1d55693f11e699f2b0f14a53a
isabeela <isabeelabg@gmail.com>
2021-11-03 11:00:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-on-fire-3mmvs5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-on-fire-3mmvs5)
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// imports google firebase//
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AuthService } from './auth.service';
+import { FirelistComponent } from './firelist/firelist.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'firelist', component: FirelistComponent },
+    ]),
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBwPl3lfpzet1oqx8f3em_ghcJtxwbyIjc',
+      authDomain: 'angularlist-d53cd.firebaseapp.com',
+      projectId: 'angularlist-d53cd',
+      storageBucket: 'angularlist-d53cd.appspot.com',
+      messagingSenderId: '39091109327',
+      appId: '1:39091109327:web:dd6ed6bf065683f0ea26ba',
+    }),
+    AngularFireAuthModule,
+  ],
+  declarations: [AppComponent, HelloComponent, FirelistComponent],
+  bootstrap: [AppComponent],
+  providers: [AuthService],
+})
+export class AppModule {}
